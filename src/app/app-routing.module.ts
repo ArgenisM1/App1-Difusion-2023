8e42926e33124b2c70e6ffe244e0abf78a249453
unknown <argenishalo117@gmail.com>
2023-09-20 16:54:54 -0600

    20/09/2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{ InicioComponent } from './componentes/pages/Inicio/inicio.component';
+import{ GaleriaComponent } from './componentes/pages/Galeria/galeria.component';
+import{ DatosComponent } from './componentes/pages/Datos/datos.component';
+import{ MapasComponent } from './componentes/pages/Mapas/mapas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Inicio',component:InicioComponent},
+  {path:'Galeria',component:GaleriaComponent},
+  {path:'Datos',component:DatosComponent},
+  {path:'Mapas',component:MapasComponent},
+  {path:'**',component:InicioComponent},
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
